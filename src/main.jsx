323a13ff342f8a071682522a9c33f615ffe2e7d9
githubscientist <sathish.datascientist@gmail.com>
2024-08-01 11:01:15 +0530

    moving App component to its own file

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-function App() {
-  return <h1>Hello World!</h1>
-}
+import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
