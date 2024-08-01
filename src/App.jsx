45917c1926da1baa86543f63d3d32c0b2ce831ae
githubscientist <sathish.datascientist@gmail.com>
2024-08-01 11:59:05 +0530

    working with array lists --using map method

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,13 @@
 function App({ notes }) {
 
-    // create an array of React list items
-    const notesList = [];
-
-    for (let i = 0; i < notes.length; i++){
-        notesList.push(<li>{ notes[i].content }</li>);
-    }
-
-    console.log(notesList);
-
   return (
       <div>
         <h1>Notes</h1>
         <ul>
             {
-                notesList      
+                notes.map((note) => {
+                    return <li>{ note.content }</li>
+                })      
             }
         </ul>      
     </div>
