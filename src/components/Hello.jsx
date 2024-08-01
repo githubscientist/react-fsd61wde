1e12fdbb09a1eba5b28222a8d3ae6333b1064a3a
githubscientist <sathish.datascientist@gmail.com>
2024-08-01 11:12:53 +0530

    adding javascript to the component

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,5 +1,15 @@
 function Hello() {
-    return <h1>Hello World!</h1>
+    const a = 10;
+    const b = 20;
+
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <p>
+                {a} plus {b} is {a + b}
+            </p>
+        </div>
+    )
 }
 
 export default Hello;
