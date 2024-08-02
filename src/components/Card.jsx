d4aad426ab2a77926170a097ca340b72dc48d9c1
githubscientist <sathish.datascientist@gmail.com>
2024-08-02 10:50:43 +0530

    task1 from react day 2 explained

---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+import './Card.css';
+
+function Card() {
+  return (
+    <div className="card">
+          <h1>$0/month</h1>
+          <hr></hr>
+          <p>Single User</p>
+          <p>5GB Storage</p>
+          <p>Unlimited Public Projects</p>
+          <p>Community Access</p>
+          <p>Unlimited Private Projects</p>
+          
+    </div>
+  )
+}
+
+export default Card;
