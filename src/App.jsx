10065a7c17bda4544fb74998433fa9f051996780
githubscientist <sathish.datascientist@gmail.com>
2024-08-02 11:53:44 +0530

    creating multiple components --class components

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,36 @@
-// Create a Functional Component called App
-// function App() {
-//   return (
-//     <div>
-//       <h1>Hello World</h1>
-//     </div>
-//   )
-// }
-
-// export default App;
-
-// Create a Class Component called App
-
 import { Component } from "react";
+
+class Header extends Component {
+  render() {
+    return <h1>Hello World</h1>
+  }
+}
+
+class Description extends Component {
+  render() {
+    return <p>A message to the world that we are here!</p>
+  }
+}
+
+
+class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Description />
+      </div>
+    )
+  }
+}
+
 class App extends Component{
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <Hello />
+        <Hello />
+        <Hello />
       </div>
     )
   }
