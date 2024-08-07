3d5f185d7fff62c2e4e8e9ab4d85657bf1bed995
githubscientist <sathish.datascientist@gmail.com>
2024-08-07 11:52:11 +0530

    lifting the state up

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import { useState } from "react";
 import B from "./components/B";
 
 const App = () => {
 
+  const [name, setName] = useState('');
+
   // 1. Create a function named fn that takes an argument.
   const fn = (arg) => {
-    console.log(arg);
+    // console.log(arg);
+    setName(arg);
   }
 
   return (
     <div>
+      <p>Data From Child: { name }</p>
       <B 
         // 2. Pass the function fn as a prop to the component B.
         fn={fn}
