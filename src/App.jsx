6b8e7739d60cb0cb7d00ab156795832c86c2f59b
githubscientist <sathish.datascientist@gmail.com>
2024-08-08 12:26:03 +0530

    basic counter application

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,38 @@
+import { useState } from "react";
+
 const App = () => {
+
+  const [count, setCount] = useState(0);
+
+  const style = {
+    display: 'flex',
+    justifyContent: 'space-around',
+    marginTop: '40px'
+  }
+
+  const increase = () => {
+    setCount(count + 1);
+  }
+
+  const decrease = () => {
+    setCount(count - 1);
+  }
+
+  const reset = () => {
+    setCount(0);
+  }
+
   return (
-    <div>App</div>
+    <div>
+      <div style={style}>
+        <button onClick={reset}>reset</button>
+      </div>
+      <div style={style}>
+        <button onClick={increase}>plus</button>
+        <div>{ count }</div>
+        <button onClick={decrease}>minus</button>
+      </div>
+    </div>
   )
 }
 
