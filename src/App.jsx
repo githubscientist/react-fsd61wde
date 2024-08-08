67ad41b9109d9770dd13f05ee4716f15cca36d37
githubscientist <sathish.datascientist@gmail.com>
2024-08-08 10:35:16 +0530

    setting up the repo for react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,7 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./components/Home";
-import About from "./components/About";
-import Nav from "./components/Nav";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Nav />,
-    errorElement: <div><h1>404 Not Found</h1></div>,
-    children: [
-      {
-        path: "",
-        element: <Home />
-      },
-      {
-        path: "about/:userId",
-        element: <About />
-      }
-    ]
-  }
-])
-
 const App = () => {
-  return <RouterProvider router={router} />
+  return (
+    <div>App</div>
+  )
 }
 
 export default App;
