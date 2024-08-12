bac37010f2ddbe6e31520b2af6361ebcd64f7d53
githubscientist <sathish.datascientist@gmail.com>
2024-08-12 12:32:58 +0530

    moving reducers to its own file

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,6 @@
-import { useReducer } from "react";
+import React, { useReducer } from "react";
+import { initialState, reducer } from "./reducers/counterReducer";
 
-const initialState = {
-  count: 0,
-  clicks: []
-};
-
-function reducer(state, action) {
-  // reducer function is called when dispatch is called
-  // action will point to the object that is passed in dispatch
-  // state argument will be the current state of the component
-  // expected from reducer function is to return the new state
-  switch (action.type) {
-    case 'INCREMENT':
-      return {
-        count: state.count + 1,
-        clicks: [...state.clicks, 'INC']
-      };
-    
-    case 'DECREMENT':
-      return {
-        count: state.count - 1,
-        clicks: [...state.clicks, 'DEC']
-      };
-    
-    case 'RESET':
-      return {
-        count: 0,
-        clicks: [...state.clicks, 'RES']
-      };
-  }
-}
 
 const App = () => {
 
