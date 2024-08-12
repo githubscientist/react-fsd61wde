5fa0212f43ff713dbad4aeed60cb5f57975e5c67
githubscientist <sathish.datascientist@gmail.com>
2024-08-12 11:33:11 +0530

    useReducer hook --counter example

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
-import { useState } from "react";
+import { useReducer } from "react";
+
+const initialState = 0;
+
+function reducer(state, action) {
+  // reducer function is called when dispatch is called
+  // action will point to the object that is passed in dispatch
+  // state argument will be the current state of the component
+  // expected from reducer function is to return the new state
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    
+    case 'DECREMENT':
+      return state - 1;
+  
+    case 'RESET':
+      return initialState;
+  }
+}
 
 const App = () => {
 
-  const [count, setCount] = useState(0);
-
+  const [count, dispatch] = useReducer(reducer, initialState);
 
   const increment = () => {
-    // increment the count
-    setCount(count + 1);
+    dispatch({ type: 'INCREMENT' });
   }
 
   const decrement = () => {
-    setCount(count - 1);
+    dispatch({ type: 'DECREMENT' });
   }
 
   const reset = () => {
-    setCount(0);
+    dispatch({ type: 'RESET' });
   }
 
   return (
