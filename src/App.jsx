f072d10fbee83a9427f45f11d711d8cebdfa519e
githubscientist <sathish.datascientist@gmail.com>
2024-08-12 10:49:48 +0530

    using useRef hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+import { useRef } from "react";
+
 const App = () => {
+
+  const inputRef = useRef(null);
+
+  const handleFocus = () => {
+    // move the focus to the input box
+    inputRef.current.focus();
+    // console.log(inputRef.current.value);
+    inputRef.current.value = "Hello World";
+  }
+
   return (
-    <div>App</div>
+    <div>
+      <input 
+        type="text"
+        placeholder="Write something..."
+        ref={inputRef}
+      />
+      <button onClick={handleFocus}>Focus Input</button>
+    </div>
   )
 }
 
