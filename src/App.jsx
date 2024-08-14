8bda005a7138face45e3a606bdef12494b8073f7
githubscientist <sathish.datascientist@gmail.com>
2024-08-14 13:19:34 +0530

    formik example

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,53 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Todos from "./components/Todos";
-import todosLoader from "./loaders/todosLoader";
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Todos />,
-    loader: todosLoader
-  }
-]);
+import { useFormik } from "formik";
 
 const App = () => {
-  return <RouterProvider router={router} />;
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: ''
+    },
+    onSubmit: values => {
+      console.log(values);
+    },
+    validate: values => {
+      const errors = {};
+
+      if (!values.username) {
+        errors.username = 'Required';
+      }
+
+      return errors;
+    }
+  });
+
+  return (
+    <div>
+      <form onSubmit={formik.handleSubmit}>
+        <input
+          type="text" 
+          username="username"
+          value={formik.values.username}
+          onChange={formik.handleChange}
+          placeholder="Username"
+          name="username"
+        /> { formik.errors.username }
+        <br />
+        <br />
+        <input
+          type="password"
+          password="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          placeholder="Password"
+          name="password"
+        />
+        <br />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  )
 }
 
 export default App;
--- src/loaders/todosLoader.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const todosLoader = async () => {
-    const response = await axios.get('https://66baf2d46a4ab5edd636a422.mockapi.io/todos');
-    return response.data;
-}
-
-export default todosLoader;
