f1a25835d8cc3ce0e2846730fcaa79df4ee97bf6
githubscientist <sathish.datascientist@gmail.com>
2024-08-16 10:50:32 +0530

    form handling without formik

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,48 @@
-import { useFormik } from "formik";
+import { useRef, useState } from "react";
 
+// Handling form without formik library
 const App = () => {
 
-  const formik = useFormik({
-    initialValues: {
-      username: '',
-      password: ''
-    },
-    onSubmit: values => {
-      console.log(values);
-    },
-    validate: values => {
-      const errors = {};
-
-      if (!values.username) {
-        errors.username = 'Required';
-      }
-
-      return errors;
-    }
+  const [formData, setFormData] = useState({
+    name: '',
+    email: ''
   });
+  const ref = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData); // will run whenever any state changes
+
+    // reset the values after submit
+    setFormData({
+      name: '',
+      email: ''
+    });
+
+    // focus on name input field after submit
+    ref.current.focus();
+  }
 
   return (
     <div>
-      <form onSubmit={formik.handleSubmit}>
-        <input
-          type="text" 
-          username="username"
-          value={formik.values.username}
-          onChange={formik.handleChange}
-          placeholder="Username"
-          name="username"
-        /> { formik.errors.username }
-        <br />
-        <br />
-        <input
-          type="password"
-          password="password"
-          value={formik.values.password}
-          onChange={formik.handleChange}
-          placeholder="Password"
-          name="password"
-        />
-        <br />
-        <br />
-        <button type="submit">Submit</button>
+      <h1>Subscribe to our newsletter</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" name="name" 
+            value={formData.name}
+            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+            ref={ref}
+          />
+        </label>
+        <label>
+          Email:
+          <input type="email" name="email" 
+            value={formData.email}
+            onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+          />
+        </label>
+        <button type="submit">Subscribe</button>
       </form>
     </div>
   )
