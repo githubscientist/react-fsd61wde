6bb8ea4c452f45cfd794b239369c87edd2b4cfcb
githubscientist <sathish.datascientist@gmail.com>
2024-08-16 11:02:28 +0530

    form handling using formik library

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,39 @@
+import { useFormik } from "formik";
 import { useRef, useState } from "react";
 
 // Handling form without formik library
 const App = () => {
 
-  const [formData, setFormData] = useState({
-    name: '',
-    email: ''
-  });
-  const ref = useRef(null);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(formData); // will run whenever any state changes
-
-    // reset the values after submit
-    setFormData({
+  const formik = useFormik({
+    initialValues: {
       name: '',
       email: ''
-    });
+    },
+    onSubmit: (values, { resetForm }) => {
+      console.log(values);
+      resetForm();
+    }
+  });
 
-    // focus on name input field after submit
-    ref.current.focus();
-  }
+  const ref = useRef(null);
 
   return (
     <div>
       <h1>Subscribe to our newsletter</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={formik.handleSubmit}>
         <label>
           Name:
           <input type="text" name="name" 
-            value={formData.name}
-            onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+            value={formik.values.name}
+            onChange={formik.handleChange}
             ref={ref}
           />
         </label>
         <label>
           Email:
           <input type="email" name="email" 
-            value={formData.email}
-            onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+            value={formik.values.email}
+            onChange={formik.handleChange}
           />
         </label>
         <button type="submit">Subscribe</button>
